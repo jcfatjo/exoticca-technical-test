eea27e8b250a224112c2cae50c1f9880af2ea67d
José Carlos Fatjó García <jc.fatjo@outlook.com>
2024-01-29 13:23:37 +0100

    chore(chakra-ui): add chakra-ui.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import Providers from "@/wrappers/Providers";
 import type { AppProps } from "next/app";
+import React from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
+const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
     return (
         <Providers>
             <Component {...pageProps} />
         </Providers>
     );
-}
+};
+
+export default App;
--- src/wrappers/Providers/index.tsx
+++ src/wrappers/Providers/index.tsx
@@ -1,10 +1,15 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { PropsWithChildren } from "react";
+import React, { PropsWithChildren } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const queryClient = new QueryClient();
 
 const Providers: React.FC<PropsWithChildren> = ({ children }) => {
-    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ChakraProvider>{children}</ChakraProvider>
+        </QueryClientProvider>
+    );
 };
 
 export default Providers;
